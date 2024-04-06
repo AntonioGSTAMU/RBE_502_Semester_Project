df4e77e5a43da07af5e08e7869523cae05b49a0c
expert-amateur <mishra.tanish.tm@gmail.com>
2024-04-06 18:30:02 -0400

    Working lqr implementation, not catching the uav

--- quadrotor_sim/lqr_example.m
+++ quadrotor_sim/lqr_example.m
@@ -10,23 +10,55 @@ m = 0.5;  % Total mass of the quadrotor [kg]
 I = [1.24, 1.24, 2.48];  % Mass moment of inertia [kg m^2]
 mu = 3.0;  % Maximum thrust of each rotor [N]
 sigma = 0.01;  % The proportionality constant relating thrust to torque [m]
-
+p=[g,l,m,sigma];
 quad = quadrotor(g, l, m, diag(I), mu, sigma);
+n=[0,0,0]';
+r=[0,0,0]';
+
+%[A, B] = linearize(p, I, n, r);
+A = [0	0	0	0	0	0	1	0	0	0	0	0;
+     0	0	0	0	0	0	0	1	0	0	0	0;
+     0	0	0	0	0	0	0	0	1	0	0	0;
+     0	0	0	0	0	0	0	0	0	1	0	0;
+     0	0	0	0	0	0	0	0	0	0	1	0;
+     0	0	0	0	0	0	0	0	0	0	0	1;
+     0	0	0	0	19.62	0	0	0	0	0	0	0;
+     0	0	0	-19.62	0	0	0	0	0	0	0	0;
+     0	0	0	0	0	0	0	0	0	0	0	0;
+     0	0	0	0	0	0	0	0	0	0	0	0;
+     0	0	0	0	0	0	0	0	0	0	0	0;
+     0	0	0	0	0	0	0	0	0	0	0	0];
+B = [0                           0                           0                           0;
+     0                           0                           0                           0;
+     0                           0                           0                           0;
+     0                           0                           0                           0;
+     0                           0                           0                           0;
+     0                           0                           0                           0;
+     0                           0                           0                           0;
+     0                           0                           0                           0;
+     2                           2                           2                           2;
+     0                           0.0203873598369011         0                          -0.0203873598369011;
+     -1                          0                           1                           0;
+     0.0200000000000000        -0.0200000000000000         0.0200000000000000        -0.0200000000000000];
+
 
 % INTRUDER
-path = @(t) [cos(t); sin(t); 2];
+path = @(t) [2*cos(0.5*t); 0.5*sin(0.5*t); 2];
 dist = struct("r", @(t,z)0.1*[sin(t); sin(2*t); sin(4*t)],...
     "n", @(t,z) 0.1*[0.1; 0.01; 0.1]);
  
 intruder = uav(path, dist);
-
+Q = diag([1000,1000, 1000, 100, 100, 100, 100, 100, 100, 10000, 10000, 10000]);
+R = eye(4)*0.1*100 ;
 % CONTROLLER
-ctrl = lqr_control(target, quad, A,B,Q,R);
+%target=[1,1,2]';
+
+ctrl = lqr_control(quad, A,B,Q,R);
 
 % SIMULATION
 
 sim = simulator(quad, ctrl, intruder);
-sim.simtime = [0 5];
+sim.simtime = [0 20];
 sim.timestep = 0.01;
 sim.epsilon = 0.1;
 
--- quadrotor_sim/src/lqr_control.m
+++ quadrotor_sim/src/lqr_control.m
@@ -1,20 +1,24 @@
 classdef lqr_control < handle
     properties(Access = public)
-        target(3,1) double;
-        k(1,2) double;
-        u0(1,1) double;
+        %target(1,3) double;
+        k(4,12) double;
+        u0(4,1) double;
+        y_history(3,3) double
     end
 
 
     methods(Access = public)
-        function obj = lqr_control(target_coords, quadrotor, A, B, Q, R)
+        function obj = lqr_control(quadrotor, A, B, Q, R) %target_coords
             obj.u0 = quadrotor.m*quadrotor.g/4;
-            obj.target = target_coords;
+            %obj.target = target_coords;
+            %obj.target = obj.target';
             obj.k = lqr(A,B,Q,R);
         end
 
-        function u = output(obj, ~, z, ~) %z should be a 12x1 column vector
-            u = obj.u0 + (-obj.k*[(z(1:3)-obj.target);z(4:12)]); %repmat(obj.k*[(obj.altitude - z(3)); -z(9)],[4,1]);
+        function u = output(obj, ~, z, y0)%z should be a 12x1 column vector
+            %obj.y_history=[];
+            %yt=
+            u = obj.u0 + (-obj.k*(z-[y0;zeros(9,1)])); %u = obj.u0 + (-obj.k*[(z(1:3)-obj.target);z(4:12)]); %repmat(obj.k*[(obj.altitude - z(3)); -z(9)],[4,1]);
         end
     end
 
