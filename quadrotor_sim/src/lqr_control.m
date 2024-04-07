76fe7c41d2eace217f5cf52065fb06f0cd02f0fd
expert-amateur <mishra.tanish.tm@gmail.com>
2024-04-07 14:15:44 -0400

    Partially working first order prediction

--- quadrotor_sim/src/lqr_control.m
+++ quadrotor_sim/src/lqr_control.m
@@ -5,6 +5,7 @@ classdef lqr_control < handle
         u0(4,1) double;
         y_history double;
         count(1,1) double;
+        mindist (1,1) double
     end
 
 
@@ -16,19 +17,24 @@ classdef lqr_control < handle
             obj.k = lqr(A,B,Q,R);
             obj.count=0;
             obj.y_history=zeros(3,3);
+            obj.mindist=1000;
         end
 
-        function u = output(obj, ~, z, y0)%z should be a 12x1 column vector
+        function u = output(obj, isCaptured, z, y0)%z should be a 12x1 column vector
             obj.y_history(:,1)=[];
             obj.y_history=[obj.y_history,y0];
-            if obj.count<3
-                yt=y0;
-                obj.count=obj.count+1;
+            if isCaptured==false
+                if obj.count<3 
+                    yt=y0;
+                    obj.count=obj.count+1;
+                else
+                    yt=y0+110*(obj.y_history(:,3)-obj.y_history(:,2));
+                end
             else
-                yt=y0+0.05*(obj.y_history(3)-obj.y_history(2));
+                yt=[0 0 0]';
             end
             
-            disp(yt-y0)
+            %disp()
             %obj.y_history=[];
             %yt=
             u = obj.u0 + (-obj.k*(z-[yt;zeros(9,1)])); %u = obj.u0 + (-obj.k*[(z(1:3)-obj.target);z(4:12)]); %repmat(obj.k*[(obj.altitude - z(3)); -z(9)],[4,1]);
