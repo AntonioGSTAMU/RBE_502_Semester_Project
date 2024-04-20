e6838f3f2830b9d6cbecb183161fbdc8b3410d7a
expert-amateur <mishra.tanish.tm@gmail.com>
2024-04-20 18:43:12 -0400

    Quadratic Traj prediction

--- quadrotor_sim/src/lqr_control_v1.m
+++ quadrotor_sim/src/lqr_control_v1.m
@@ -1,4 +1,4 @@
-classdef lqr_control < handle
+classdef lqr_control_v1 < handle
     properties(Access = public)
         %target(1,3) double;
         k(4,12) double;
@@ -17,8 +17,8 @@ classdef lqr_control < handle
             %obj.target = obj.target';
             obj.k = lqr(A,B,Q,R);
             obj.count=0;
-            obj.y_hist_size=15;
-            obj.y_history=zeros(3, obj.y_hist_size);
+            obj.y_hist_size=5;
+            obj.y_history=zeros(3,3);
             obj.mindist=1000;
         end
 
@@ -32,48 +32,26 @@ classdef lqr_control < handle
             del_y=100*(del_y_2+del_y_dash);
         end
 
-        function y_pred= curve_fit_2(obj, pointset, delta_t)
-            %disp(size(pointset));
-            datalen=size(pointset,2);
-    
-
-            output_x=pointset(1,:);
-            output_y=pointset(2,:);
-            output_z=pointset(3,:);
-            input_t=0:datalen-1;
-
-            coeff_x=polyfit(input_t,output_x,2);
-            coeff_y=polyfit(input_t,output_y,2);
-            coeff_z=polyfit(input_t,output_z,2);
-
-            pred_x=polyval(coeff_x,input_t(end)+delta_t);
-            pred_y=polyval(coeff_y,input_t(end)+delta_t);
-            pred_z=polyval(coeff_z,input_t(end)+delta_t);
-
-            y_pred=[pred_x,pred_y,pred_z]';
-
-        end
-
         function u = output(obj, isCaptured, z, y0) %z should be a 12x1 column vector
             obj.y_history(:,1)=[];
             obj.y_history=[obj.y_history,y0];
-            delta_t=110;
-            
-            %pred_w=0.8;
             if isCaptured==false
                 if obj.count<obj.y_hist_size 
                     yt=y0;
                     obj.count=obj.count+1;
                 else
-                    %yt=y0+obj.second_order_pred(obj.y_history(:,1),obj.y_history(:,2),obj.y_history(:,3));%y0+110*(obj.y_history(:,2)-obj.y_history(:,3));
-                    %yt=obj.curve_fit_2(obj.y_history, delta_t);
-                    yt=obj.curve_fit_2(obj.y_history, delta_t);
+                    yt=y0+obj.second_order_pred(obj.y_history(:,1),obj.y_history(:,2),obj.y_history(:,3));%y0+110*(obj.y_history(:,2)-obj.y_history(:,3));
                 end
             else
                 yt=[0 0 0]';
             end
             
-            u = obj.u0 + (-obj.k*(z-[yt;zeros(9,1)]));
+            %disp(yt-y0);
+            %obj.y_history=[];
+            %yt=
+            %const_vel_factor=-0.1*normalize(y0-z(1:3));
+            u = obj.u0 + (-obj.k*(z-[yt;zeros(9,1)])); %u = obj.u0 + (-obj.k*[(z(1:3)-obj.target);z(4:12)]); %repmat(obj.k*[(obj.altitude - z(3)); -z(9)],[4,1]);
+            %u = obj.u0 + (-obj.k*(z-[yt;zeros(3,1);const_vel_factor;zeros(3,1)]));%
         end
     end
 
